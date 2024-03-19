8ce31b05c8dd9e6679479369aff947720ebe7d35
Abhay Malvi <abhaymalvi@Abhays-MacBook-Air.local>
2024-03-19 16:16:54 +0530

    changes 7.0

--- db_connection.py
+++ db_connection.py
@@ -5,5 +5,5 @@ conn = MongoClient(os.environ.get("DB_URL"))
 if conn:
     db = conn.get_database("pool")
 
-    users_collection = db.get_collection("users2")
-    tasks_collection = db.get_collection("tasks2")
+    users_collection = db.get_collection("users")
+    tasks_collection = db.get_collection("tasks")
