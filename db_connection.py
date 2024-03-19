130df434db3b1810c803eeb4c2467f4a7e793728
Abhay Malvi <abhaymalvi@Abhays-MacBook-Air.local>
2024-03-19 15:59:01 +0530

    changes 6.0

--- db_connection.py
+++ db_connection.py
@@ -3,9 +3,15 @@ import os
 
 conn = MongoClient(os.environ.get("DB_URL"))
 if conn:
-    print(conn)
+    if "pool" not in conn.list_database_names():
+        conn.create_database("pool")
+    db = conn.get_database("pool")
 
-    db = conn.get_database("taskify")
+    if "users" not in db.list_collection_names():
+        db.create_collection("users")
 
+    if "tasks" not in db.list_collection_names():
+        db.create_collection("tasks")
+        
     users_collection = db.get_collection("users")
     tasks_collection = db.get_collection("tasks")
